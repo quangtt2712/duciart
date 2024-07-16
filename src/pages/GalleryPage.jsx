dd1d27786dc8e6fc9bb88fd99d7d9356fb0955ef
hieugo6789 <hieugo240103@gmail.com>
2024-07-16 15:23:05 +0700

    fix gallery page

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,47 +1,103 @@
-import T01 from "../assets/images/gallery-big-1.jpg";
-import T02 from "../assets/images/gallery-big-2.jpg";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
 
-const List_Image = [
+const itemData = [
   {
-    id: 1,
-    image_src: T01,
-    alt: "Phương, 10 years",
-    owner: "Phương, 10 years",
+    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    title: "Bed",
+    author: "swabdesign",
   },
   {
-    id: 2,
-    image_src: T02,
-    alt: "Hiếu, 10 years",
-    owner: "Phương, 10 years",
+    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    title: "Books",
+    author: "Pavel Nekoranec",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    title: "Sink",
+    author: "Charles Deluvio",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    title: "Kitchen",
+    author: "Christian Mackie",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    title: "Blinds",
+    author: "Darren Richardson",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    title: "Chairs",
+    author: "Taylor Simpson",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    title: "Laptop",
+    author: "Ben Kolde",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    title: "Doors",
+    author: "Philipp Berndt",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    title: "Coffee",
+    author: "Jen P.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    title: "Storage",
+    author: "Douglas Sheppard",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    title: "Candle",
+    author: "Fi Bell",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    title: "Coffee table",
+    author: "Hutomo Abrianto",
   },
 ];
 
 const GalleryPage = () => {
   return (
-    <section id="content">
-      <div className="container_12">
-        <div className="block-1 top-5">
-          <div className="block-1-shadow">
-            <h2 className="clr-6 p4">Tranh của học viên</h2>
-            <div className="pag">
-              <div style={{ paddingBottom: "10px" }}>
-                <ul>
-                  {List_Image.map((image) => (
-                    <li key={image.id}>
-                      <span>
-                        <div className="text-3">{image.owner}</div>
-                        <img
-                          className="img-border img-gallery"
-                          src={image.image_src}
-                          alt={image.alt}
-                        />
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
+    <section id="content1">
+      <div className="block-1 top-5">
+        <div
+          className="block-1-shadow"
+          style={{ paddingBottom: "50px" }}
+        >
+          <h2 className="clr-6 p4">Tranh của học viên</h2>
+          <Box sx={{ height: "auto" }}>
+            <ImageList
+              variant="masonry"
+              cols={3}
+              gap={8}
+            >
+              {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                  <img
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    alt={item.title}
+                    loading="lazy"
+                    style={{ borderRadius: "10px" }}
+                  />
+                  <ImageListItemBar
+                    position="below"
+                    title={item.author}
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Box>
         </div>
       </div>
     </section>
