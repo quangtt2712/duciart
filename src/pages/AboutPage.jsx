53dd9748df9c99643ae6c509c90f5d35072f98b3
hieugo6789 <hieugo240103@gmail.com>
2024-07-23 23:12:48 +0700

    update about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
 import imgHome from "../assets/images/page2-img1.jpg";
+import imgVision from "../assets/images/vision.png";
+import imgDuty from "../assets/images/duty.png";
 
 const AboutPage = () => {
   return (
@@ -42,7 +44,6 @@ const AboutPage = () => {
                 </p>
               </div>
             </div>
-
             <p className="clr-6 text-4">
               Nhưng mong rằng, khi đến với Duci thì ai cũng giống nhau, không
               lấy tuổi tác, vị thế làm rào cản. Vì chúng ta đều mang trong mình
@@ -51,40 +52,76 @@ const AboutPage = () => {
               bạn. Để từ đó sinh sôi những gam màu tuyệt vời nhất !
             </p>
             <p
-              className="clr-6"
-              style={{ fontSize: "30px" }}
+              className="clr-4"
+              style={{
+                fontSize: "30px",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              <strong>Tầm nhìn - Sứ mệnh</strong>
+              <img
+                src={imgVision}
+                style={{ height: "64px", marginRight: "10px" }}
+              />
+              <strong>Tầm nhìn</strong>
             </p>
-            <p className="clr-6 text-4">
-              Tại Duci Art, chúng tôi mong muốn xây dựng một thế giới nơi mọi
-              trẻ em đều có thể khám phá và thể hiện sự sáng tạo của mình một
-              cách tự do. Chúng tôi hướng đến việc nuôi dưỡng một thế hệ những
-              nghệ sĩ trẻ có thể nhìn thế giới qua lăng kính của sự sáng tạo và
-              đổi mới, mang màu sắc và trí tưởng tượng đến mọi khía cạnh của
-              cuộc sống. <br />
-              <br />
-              Mục tiêu của chúng tôi là tạo ra một môi trường bao dung, truyền
-              cảm hứng và nuôi dưỡng, nơi trẻ em có thể phát triển tài năng nghệ
-              thuật, xây dựng sự tự tin và khám phá niềm vui của việc thể hiện
-              nghệ thuật. <br />
-              <br />
-              Sứ mệnh của chúng tôi tại Duci Art là truyền cảm hứng và trao
-              quyền cho trẻ em thông qua nghệ thuật. Chúng tôi cam kết cung cấp
-              giáo dục mỹ thuật chất lượng cao, giúp phát triển khả năng sáng
-              tạo và niềm đam mê học hỏi suốt đời. Thông qua chương trình học có
-              cấu trúc nhưng linh hoạt, chúng tôi cung cấp một loạt các lớp học
-              vẽ ở nhiều lĩnh vực khác nhau, từ vẽ chì, sáp dầu, màu bột đến kí
-              họa.
-              <br />
-              <br />
-              Ngoài việc dạy các kỹ năng nghệ thuật cơ bản, chúng tôi còn nỗ lực
-              tạo ra một cộng đồng hỗ trợ nơi trẻ em cảm thấy được trân trọng và
-              truyền cảm hứng. Chúng tôi cam kết làm cho nghệ thuật trở nên dễ
-              tiếp cận với tất cả trẻ em, bất kể hoàn cảnh của các em, và nuôi
-              dưỡng tiềm năng nghệ thuật trong mỗi em. Thông qua các chương
-              trình của mình, chúng tôi hy vọng sẽ khơi dậy niềm đam mê nghệ
-              thuật, điều sẽ tiếp tục phát triển trong suốt cuộc đời của các em.
+            <p className="clr-6 text-4 text-decoration">
+              <p>
+                • Tại Duci Art, chúng tôi mong muốn xây dựng một thế giới nơi
+                mọi trẻ em đều có thể khám phá và thể hiện sự sáng tạo của mình
+                một cách tự do.
+              </p>
+              <p>
+                • Chúng tôi hướng đến việc nuôi dưỡng một thế hệ những nghệ sĩ
+                trẻ có thể nhìn thế giới qua lăng kính của sự sáng tạo và đổi
+                mới, mang màu sắc và trí tưởng tượng đến mọi khía cạnh của cuộc
+                sống.
+              </p>
+              <p>
+                • Mục tiêu của chúng tôi là tạo ra một môi trường bao dung,
+                truyền cảm hứng và nuôi dưỡng, nơi trẻ em có thể phát triển tài
+                năng nghệ thuật, xây dựng sự tự tin và khám phá niềm vui của
+                việc thể hiện nghệ thuật.
+              </p>
+            </p>
+            <p
+              className="clr-4"
+              style={{
+                fontSize: "30px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={imgDuty}
+                style={{ height: "64px", marginRight: "10px" }}
+              />
+              <strong>Sứ mệnh</strong>
+            </p>
+            <p className="clr-6 text-4 text-decoration">
+              <p>
+                • Sứ mệnh của chúng tôi tại Duci Art là truyền cảm hứng và trao
+                quyền cho trẻ em thông qua nghệ thuật.
+                <p>
+                  • Chúng tôi cam kết cung cấp giáo dục mỹ thuật chất lượng cao,
+                  giúp phát triển khả năng sáng tạo và niềm đam mê học hỏi suốt
+                  đời. Thông qua chương trình học có cấu trúc nhưng linh hoạt,
+                  chúng tôi cung cấp một loạt các lớp học vẽ ở nhiều lĩnh vực
+                  khác nhau, từ vẽ chì, sáp dầu, màu bột đến kí họa.
+                </p>
+              </p>
+              <p>
+                • Ngoài việc dạy các kỹ năng nghệ thuật cơ bản, chúng tôi còn nỗ
+                lực tạo ra một cộng đồng hỗ trợ nơi trẻ em cảm thấy được trân
+                trọng và truyền cảm hứng.
+                <p>
+                  • Chúng tôi cam kết làm cho nghệ thuật trở nên dễ tiếp cận với
+                  tất cả trẻ em, bất kể hoàn cảnh của các em, và nuôi dưỡng tiềm
+                  năng nghệ thuật trong mỗi em. Thông qua các chương trình của
+                  mình, chúng tôi hy vọng sẽ khơi dậy niềm đam mê nghệ thuật,
+                  điều sẽ tiếp tục phát triển trong suốt cuộc đời của các em.
+                </p>
+              </p>
             </p>
           </div>
         </div>
